0902783116e4ce0c3a3273014fcea8e49184d77a
Hendriyan Ogivano <hogivano@gmail.com>
2022-04-15 19:51:41 +0700

    Profile website

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-ZJ7JGYSW0D",
+        trackingId: "YOUR_GOOGLE_TRACKING_ID",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
